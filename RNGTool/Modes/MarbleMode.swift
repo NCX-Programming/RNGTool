ecb382bd6d21f2e832f54eb6638b96b4b67635c4
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-09-27 16:22:48 -0400

    Base for Marble mode

--- RNGTool/Modes/MarbleMode.swift
+++ RNGTool/Modes/MarbleMode.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct MarbleMode: View {
+    @State private var numOfMarbles = 1
+    @State private var showCopy = false
+    @State private var showMarbles = false
+    @State private var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -18,6 +23,62 @@ struct MarbleMode: View {
                         .font(.title3)
                         .foregroundColor(.secondary)
                     Divider()
+                    HStack(){
+                        if(showMarbles){
+                            ZStack() {
+                                Text("")
+                                    .font(.title)
+                                Circle()
+                                    .stroke(Color.primary, lineWidth: 3)
+                            }
+                            .frame(width: 64, height: 64)
+                            if(numOfMarbles>1){
+                                ZStack() {
+                                    Text("")
+                                        .font(.title)
+                                    Circle()
+                                        .stroke(Color.primary, lineWidth: 3)
+                                }
+                                .frame(width: 64, height: 64)
+                                if(numOfMarbles>2){
+                                    ZStack() {
+                                        Text("")
+                                            .font(.title)
+                                        Circle()
+                                            .stroke(Color.primary, lineWidth: 3)
+                                    }
+                                    .frame(width: 64, height: 64)
+                                    if(numOfMarbles>3){
+                                        ZStack() {
+                                            Text("")
+                                                .font(.title)
+                                            Circle()
+                                                .stroke(Color.primary, lineWidth: 3)
+                                        }
+                                        .frame(width: 64, height: 64)
+                                        if(numOfMarbles>4){
+                                            ZStack() {
+                                                Text("")
+                                                    .font(.title)
+                                                Circle()
+                                                    .stroke(Color.primary, lineWidth: 3)
+                                            }
+                                            .frame(width: 64, height: 64)
+                                            if(numOfMarbles>5){
+                                                ZStack() {
+                                                    Text("")
+                                                        .font(.title)
+                                                    Circle()
+                                                        .stroke(Color.primary, lineWidth: 3)
+                                                }
+                                                .frame(width: 64, height: 64)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
             .padding(.leading, 12)
