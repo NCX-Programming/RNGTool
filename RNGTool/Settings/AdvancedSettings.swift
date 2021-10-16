476e31c009588ead57c2fe85c75ee3923e6e2d8a
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-10-16 15:15:27 -0400

    Finished adding advanced settings

--- RNGTool/Settings/AdvancedSettings.swift
+++ RNGTool/Settings/AdvancedSettings.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct AdvancedSettings: View {
+    @AppStorage("maxNumberDefault") private var maxNumberDefault = 100
+    @AppStorage("minNumberDefault") private var minNumberDefault = 0
+    @AppStorage("showLetterList") private var showLetterList = false
+    @AppStorage("showPoints") private var showPoints = false
+    @AppStorage("aceValue") private var aceValue = 1
+    @AppStorage("useFaces") private var useFaces = true
+    @AppStorage("forceSixSides") private var forceSixSides = false
+    @AppStorage("allowDiceImages") private var allowDiceImages = true
     @State private var showAlert = false
     
     var body: some View {
@@ -26,6 +34,14 @@ struct AdvancedSettings: View {
                     title: Text("Confirm Reset"),
                     message: Text("Are you sure you want to reset all settings to their defaults? This cannot be undone."),
                     primaryButton: .default(Text("Confirm")){
+                        maxNumberDefault = 100
+                        minNumberDefault = 0
+                        showLetterList = false
+                        showPoints = false
+                        aceValue = 1
+                        useFaces = true
+                        forceSixSides = false
+                        allowDiceImages = true
                         showAlert = false
                     },
                     secondaryButton: .cancel()
