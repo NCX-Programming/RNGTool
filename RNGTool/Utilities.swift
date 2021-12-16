c26745c751ed783015148912f0266653865264f3
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2021-12-16 17:48:57 -0500

    Removed unused code fragment that made the last build fail

--- RNGTool/Utilities.swift
+++ RNGTool/Utilities.swift
@@ -55,9 +55,3 @@ func resetMarbleSet(){
     
     showLetterList = false
 }
-
-extension UIApplication {
-    static var appVersion: String? {
-        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
-    }
-}
