01a068b3a4641c498c530814c54744859d81f794
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-09-02 21:19:42 -0400

    [All] Bump version numbers for release, added option to DevMode

--- RNGTool Mobile/DevMode.swift
+++ RNGTool Mobile/DevMode.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct DevMode: View {
     @EnvironmentObject var settingsData: SettingsData
+    @SceneStorage("NumberMode.randomNumber") private var randomNumber = 0
+    @SceneStorage("NumberMode.maxNumber") private var maxNumber = 0
+    @SceneStorage("NumberMode.minNumber") private var minNumber = 0
     @State private var historyModeInput = ""
     @State private var historyNumberInput = ""
+    @State private var randomNumberStr = ""
+    @State private var maxNumberStr = ""
+    @State private var minNumberStr = ""
     
     var body: some View {
         Form {
@@ -36,6 +42,41 @@ struct DevMode: View {
                 }) {
                     Text("Reset Table")
                 }
+            }
+            Section(header: Text("@SceneStorage Manipulation"), footer: Text("Edit the values stored in @SceneStorage for Number Mode.")) {
+                Text("randomNumber: \(randomNumber)")
+                TextField(text: $randomNumberStr, prompt: Text("Enter a number")) {
+                    Text("Numbers")
+                }
+                .onChange(of: randomNumberStr) { randomNumberStr in
+                    randomNumber = Int(randomNumberStr.prefix(19)) ?? 0
+                }
+                .onAppear {
+                    randomNumberStr = "\(randomNumber)"
+                }
+                .keyboardType(.numberPad)
+                Text("maxNumber: \(maxNumber)")
+                TextField(text: $maxNumberStr, prompt: Text("Enter a number")) {
+                    Text("Numbers")
+                }
+                .onChange(of: maxNumberStr) { maxNumberStr in
+                    maxNumber = Int(maxNumberStr.prefix(19)) ?? settingsData.maxNumberDefault
+                }
+                .onAppear {
+                    maxNumberStr = "\(maxNumber)"
+                }
+                .keyboardType(.numberPad)
+                Text("minNumber: \(minNumber)")
+                TextField(text: $minNumberStr, prompt: Text("Enter a number")) {
+                    Text("Numbers")
+                }
+                .onChange(of: minNumberStr) { minNumberStr in
+                    minNumber = Int(minNumberStr.prefix(19)) ?? settingsData.minNumberDefault
+                }
+                .onAppear {
+                    minNumberStr = "\(minNumber)"
+                }
+                .keyboardType(.numberPad)
             }
         }
         .padding(.horizontal, 3)
