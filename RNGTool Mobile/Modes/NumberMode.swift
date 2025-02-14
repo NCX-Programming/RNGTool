b16f2df76a6fa56035e43aef83475392825b8a93
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2025-02-13 23:40:56 -0500

    [iOS] Entirely redesigned number mode Landscape orientations are also now disabled on iOS, but remain enabled on iPadOS

--- RNGTool Mobile/Modes/NumberMode.swift
+++ RNGTool Mobile/Modes/NumberMode.swift
@@ -8,147 +8,136 @@
 import SwiftUI
 import CoreHaptics
 
-extension String.StringInterpolation {
-    mutating func appendInterpolation(if condition: @autoclosure () -> Bool, _ literal: StringLiteralType) {
-        guard condition() else { return }
-        appendLiteral(literal)
-    }
-}
-
 struct NumberMode: View {
     @EnvironmentObject var settingsData: SettingsData
     @Environment(\.accessibilityReduceMotion) var reduceMotion
     @SceneStorage("NumberMode.randomNumber") private var randomNumber = 0
-    @State private var maxNumber = 0
-    @State private var minNumber = 0
+    @State private var maxNumber: Int = 0
+    @State private var minNumber: Int = 0
     @State private var engine: CHHapticEngine?
-    @State private var confirmReset = false
-    @State private var showMaxEditor = false
-    @State private var showMinEditor = false
-    @State private var maxNumberInput = ""
-    @State private var minNumberInput = ""
+    @State private var confirmReset: Bool = false
+    @State private var maxNumberInput: String = ""
+    @State private var minNumberInput: String = ""
+    // Enum for the @FocusState used to dismiss the keyboard when a button is pressed
+    enum Field {
+        case maxNumber
+        case minNumber
+    }
+    @FocusState private var fieldFocused: Field?
     
     func resetGen() {
-        maxNumber = 0
         maxNumberInput = "\(settingsData.maxNumberDefault)"
-        minNumber = 0
         minNumberInput = "\(settingsData.minNumberDefault)"
         randomNumber = 0
-        withAnimation(reduceMotion ? .none : .easeInOut(duration: 0.5)) {
-            showMaxEditor = false
-            showMinEditor = false
-        }
         confirmReset = false
     }
     
     var body: some View {
-        ScrollView {
-            Text("\(randomNumber)")
-                .font(.title)
-                .padding(.bottom, 5)
-                .onAppear {
-                    if (settingsData.saveModeStates == false) { randomNumber = 0 }
-                }
-                .contextMenu {
-                    Button(action: {
-                        copyToClipboard(item: "\(randomNumber)")
-                    }) {
-                        Text("Copy")
+        GeometryReader { geometry in
+            ScrollView {
+                Text("\(randomNumber)")
+                    // This code is to make the text showing the random number as big as possible while fitting the screen, fitting above the buttons, and not truncating.
+                    .font(.system(size: 1000))
+                    .minimumScaleFactor(0.01)
+                    .lineLimit(1)
+                    .frame(width: geometry.size.width, height: geometry.size.height * 0.5, alignment: .center)
+                    .allowsTightening(true)
+                    .onAppear {
+                        if (settingsData.saveModeStates == false) { randomNumber = 0 }
                     }
-                }
-            Group() {
-                Text("Maximum: \(maxNumberInput). Tap to change.")
-                    .onTapGesture {
-                        withAnimation(reduceMotion ? .none : .easeInOut(duration: 0.3)) {
-                            showMaxEditor.toggle()
+                    .contextMenu {
+                        Button(action: {
+                            copyToClipboard(item: "\(randomNumber)")
+                        }) {
+                            Text("Copy")
+                            Image(systemName: "document.on.document")
                         }
                     }
-                    .help("Tap here to set a custom maximum number")
-                    .onAppear{
-                        maxNumberInput = "\(maxNumber)"
-                        if (maxNumber == 0 || settingsData.saveModeStates == false) { maxNumberInput = "\(settingsData.maxNumberDefault)" }
-                    }
-                if(showMaxEditor){
+                Group() {
+                    Text("Maximum")
                     TextField("Enter a number", text: $maxNumberInput)
                         .keyboardType(.numberPad)
-                }
-                Divider()
-                Text("Minimum: \(minNumber). Tap to change.")
-                    .onTapGesture {
-                        withAnimation(reduceMotion ? .none : .easeInOut(duration: 0.3)) {
-                            showMinEditor.toggle()
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal, geometry.size.width * 0.1)
+                        .focused($fieldFocused, equals: .maxNumber)
+                        .onAppear{
+                            maxNumberInput = "\(maxNumber)"
+                            if (maxNumber == 0 || settingsData.saveModeStates == false) { maxNumberInput = "\(settingsData.maxNumberDefault)" }
                         }
-                    }
-                    .help("Tap here to set a custom minimum number")
-                    .onAppear {
-                        minNumberInput = "\(minNumber)"
-                        if (minNumber == 0 || settingsData.saveModeStates == false) { minNumberInput = "\(settingsData.minNumberDefault)" }
-                    }
-                if(showMinEditor){
+                    Text("Minimum")
                     TextField("Enter a number", text: $minNumberInput)
                         .keyboardType(.numberPad)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal, geometry.size.width * 0.1)
+                        .focused($fieldFocused, equals: .minNumber)
+                        .onAppear {
+                            minNumberInput = "\(minNumber)"
+                            if (minNumber == 0 || settingsData.saveModeStates == false) { minNumberInput = "\(settingsData.minNumberDefault)" }
+                        }
                 }
-                Divider()
-            }
-            .onChange(of: maxNumberInput) { maxNumberInput in
-                maxNumber = Int(maxNumberInput.prefix(19)) ?? settingsData.maxNumberDefault
-            }
-            .onChange(of: minNumberInput) { minNumberInput in
-                minNumber = Int(minNumberInput.prefix(19)) ?? settingsData.minNumberDefault
-            }
-            HStack {
-                Button(action:{
-                    playHaptics(engine: engine, intensity: 1, sharpness: 0.5, count: 0.2)
+                .onChange(of: maxNumberInput) { maxNumberInput in
                     maxNumber = Int(maxNumberInput.prefix(19)) ?? settingsData.maxNumberDefault
+                }
+                .onChange(of: minNumberInput) { minNumberInput in
                     minNumber = Int(minNumberInput.prefix(19)) ?? settingsData.minNumberDefault
-                    if (maxNumber <= minNumber) { minNumber = settingsData.minNumberDefault }
-                    randomNumber = Int.random(in: minNumber...maxNumber)
-                    maxNumberInput="\(maxNumber)"
-                    minNumberInput="\(minNumber)"
-                    addHistoryEntry(settingsData: settingsData, results: "\(randomNumber)", mode: "Number Mode")
-                }) {
-                    Image(systemName: "play.fill")
-                        
                 }
-                .onAppear { prepareHaptics(engine: &engine) }
-                .font(.system(size: 20, weight:.bold, design: .rounded))
-                .foregroundColor(.white)
-                .padding(.horizontal)
-                .padding(5)
-                .background(Color.accentColor)
-                .cornerRadius(20)
-                .help("Generate a number")
-                Button(action:{
-                    playHaptics(engine: engine, intensity: 1, sharpness: 0.5, count: 0.1)
-                    if(settingsData.confirmGenResets){
-                        confirmReset = true
+                Spacer()
+                    .padding(.bottom, 10)
+                VStack {
+                    Button(action:{
+                        playHaptics(engine: engine, intensity: 1, sharpness: 0.75, count: 0.2)
+                        fieldFocused = .none
+                        maxNumber = Int(maxNumberInput.prefix(19)) ?? settingsData.maxNumberDefault
+                        minNumber = Int(minNumberInput.prefix(19)) ?? settingsData.minNumberDefault
+                        if (maxNumber <= minNumber) { minNumber = settingsData.minNumberDefault }
+                        randomNumber = Int.random(in: minNumber...maxNumber)
+                        maxNumberInput="\(maxNumber)"
+                        minNumberInput="\(minNumber)"
+                        addHistoryEntry(settingsData: settingsData, results: "\(randomNumber)", mode: "Number Mode")
+                    }) {
+                        Image(systemName: "play.fill")
+                            .padding(.horizontal, geometry.size.width * 0.4)
+                            .padding(.vertical, 10)
                     }
-                    else { resetGen() }
-                }) {
-                    Image(systemName: "clear.fill")
-                }
-                .font(.system(size: 20, weight:.bold, design: .rounded))
-                .foregroundColor(.white)
-                .padding(.horizontal)
-                .padding(5)
-                .background(Color.accentColor)
-                .cornerRadius(20)
-                .help("Reset custom values and output")
-                .alert(isPresented: $confirmReset){
-                    Alert(
-                        title: Text("Confirm Reset"),
-                        message: Text("Are you sure you want to reset the generator? This cannot be undone."),
-                        primaryButton: .default(Text("Confirm")){
-                            resetGen()
-                        },
-                        secondaryButton: .cancel()
-                    )
+                    .onAppear { prepareHaptics(engine: &engine) }
+                    .font(.system(size: 20, weight:.bold, design: .rounded))
+                    .foregroundColor(.white)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+                    .help("Generate a number")
+                    Button(action:{
+                        playHaptics(engine: engine, intensity: 1, sharpness: 0.75, count: 0.1)
+                        fieldFocused = .none
+                        if(settingsData.confirmGenResets){
+                            confirmReset = true
+                        }
+                        else { resetGen() }
+                    }) {
+                        Image(systemName: "clear.fill")
+                            .padding(.horizontal, geometry.size.width * 0.4)
+                            .padding(.vertical, 10)
+                    }
+                    .font(.system(size: 20, weight:.bold, design: .rounded))
+                    .foregroundColor(.white)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+                    .help("Reset custom values and output")
+                    .alert(isPresented: $confirmReset){
+                        Alert(
+                            title: Text("Confirm Reset"),
+                            message: Text("Are you sure you want to reset the generator? This cannot be undone."),
+                            primaryButton: .default(Text("Confirm")){
+                                resetGen()
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    }
                 }
             }
+            .padding(.horizontal, 3)
+            .navigationTitle("Numbers")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding(.horizontal, 3)
-        .navigationTitle("Numbers")
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
